632662afbdb76e599270679e0e341ccdd0733220
kingjamez <mcjamez@gmail.com>
2026-08-31 09:20:33 -0400

    Add vendor install notes and apt reference script

---
+++ scripts/vendor-apt_update.sh
@@ -0,0 +1,41 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install -y  git cmake g++ libboost-all-dev libgmp-dev swig python3-numpy python3-mako python3-sphinx python3-lxml doxygen libfftw3-dev libsdl1.2-dev libgsl-dev libqwt-qt5-dev libqt5opengl5-dev python3-pyqt5 liblog4cpp5-dev libzmq3-dev python3-yaml python3-click python3-click-plugins python3-zmq python3-scipy python3-gi python3-gi-cairo gir1.2-gtk-3.0 libcodec2-dev libgsm1-dev
+
+
+
+sudo apt-get -y install \
+git cmake g++ pkg-config autoconf automake libtool libfftw3-dev libusb-1.0-0-dev libusb-dev libhidapi-dev libopengl-dev \
+qtbase5-dev qtchooser libqt5multimedia5-plugins qtmultimedia5-dev libqt5websockets5-dev \
+qttools5-dev qttools5-dev-tools libqt5opengl5-dev libqt5quick5 libqt5charts5-dev \
+qml-module-qtlocation  qml-module-qtpositioning qml-module-qtquick-window2 \
+qml-module-qtquick-dialogs qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-layouts \
+libqt5serialport5-dev qtdeclarative5-dev qtpositioning5-dev qtlocation5-dev libqt5texttospeech5-dev \
+qtwebengine5-dev qtbase5-private-dev \
+libfaad-dev zlib1g-dev libboost-all-dev libasound2-dev pulseaudio libopencv-dev libxml2-dev bison flex \
+ffmpeg libavcodec-dev libavformat-dev libopus-dev doxygen graphviz
+
+
+ 
+sudo apt-get install -y   pybind11-dev python3-matplotlib libsndfile1-dev python3-pip   libsoapysdr-dev soapysdr-tools
+pip install pygccxml
+pip install pyqtgraph
+ 
+sudo apt-get install -y   libiio-dev libad9361-dev libspdlog-dev python3-packaging python3-jsonschema
+ 
+sudo apt-get install -y  cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev
+sudo apt-get install -y libgmp-dev   
+ 
+
+
+
+sudo  dpkg --configure -a 
+sudo apt-get install python3-pip -y 
+pip install  setuptools
+
+
+sudo apt-get install cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev
+
+
+  
